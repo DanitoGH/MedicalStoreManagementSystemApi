8c4ad2ffe0f44474ebb980fe518768bc0ef91260
Daniel Adjei Boateng <freshdan24@gmail.com>
2020-10-24 03:01:19 +0000

    Fixed static root error from heroku

--- hospital_store_app/settings.py
+++ hospital_store_app/settings.py
@@ -157,16 +157,16 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media'),
-]
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
+MEDIA_URL = '/images/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn/images')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
