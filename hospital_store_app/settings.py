35e0f25ab2b4e7f2c6df0b3247a2c446b805897f
Daniel Adjei Boateng <freshdan24@gmail.com>
2020-11-03 18:24:24 +0000

    signature version added

--- hospital_store_app/settings.py
+++ hospital_store_app/settings.py
@@ -181,7 +181,8 @@ AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
 
 AWS_S3_FILE_OVERWRITE = config('AWS_S3_FILE_OVERWRITE', default=False, cast=bool)
-
+AWS_S3_REGION_NAME = 'eu-west-2'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Activate Django-Heroku.
